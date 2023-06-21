728515029ccb86d0c3eb1bbf6deee19d84a8fadb
Bhama Pillutla <65846493+bhamakpillutla@users.noreply.github.com>
2023-06-20 20:41:14 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,31 @@
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
-import os
+# from wsgiref.simple_server import make_server
+# from pyramid.config import Configurator
+# from pyramid.response import Response
+# import os
 
-def hello_world(request):
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "world"
-    message = "Welcome to AWS - , " + name + "!\n"
-    return Response(message)
+# def hello_world(request):
+#     name = os.environ.get('NAME')
+#     if name == None or len(name) == 0:
+#         name = "world"
+#     message = "Welcome to AWS - , " + name + "!\n"
+#     return Response(message)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
-    with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-        app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT"))
+#     with Configurator() as config:
+#         config.add_route('hello', '/')
+#         config.add_view(hello_world, route_name='hello')
+#         app = config.make_wsgi_app()
+#     server = make_server('0.0.0.0', port, app)
+#     server.serve_forever()
+# from flask import Flask, jsonify
+# import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 200
+
+if __name__=='__main__':
+    app.run(debug=True,host='0.0.0.0', port = 5050)
